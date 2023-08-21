7c9757dda89fd56037b17062365260d7959ac53b
lijun <421498437@qq.com>
2023-08-21 23:40:53 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
-
+import Web3 from 'web3';
+import { Button } from 'antd';
 function App() {
+  const web3 = useRef();
+  useEffect(() => {
+    web3.current = new Web3(
+      Web3.givenProvider ||
+        'wss://goerli.infura.io/ws/v3/2d43c374871243fcb6b994dc16fc4d65'
+    );
+    console.log(web3.current);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">hello web3</div>;
+      <Button type="primary">主要按钮</Button>
+    </>
   );
 }
 
